77cf094cd567ed8f6ca38392fb7b4763d5d332b3
anamaraah00 <nurhasanahcantik881@gmail.com>
2026-06-10 12:52:51 +0700

    Add files via upload

--- Tugas9.9.php
+++ Tugas9.9.php
@@ -1,47 +1,53 @@
 <?php
 
 class Tabungan {
-    private $saldo; // Encapsulation: Saldo tidak bisa diakses langsung
-    protected $namaSiswa;
+private $saldo; // Encapsulation: Saldo tidak bisa diakses langsung
+protected $namaSiswa;
 
-    public function __construct($nama, $saldoAwal) {
-        $this->namaSiswa = $nama;
-        $this->saldo = $saldoAwal;
-    }
+public function __construct($nama, $saldoAwal) {
+$this->namaSiswa = $nama;
+$this->saldo = $saldoAwal;
+}
 
-    public function setor($jumlah) {
-        $this->saldo += $jumlah;
-        return "Berhasil setor Rp " . number_format($jumlah);
-    }
+public function setor($jumlah) {
+$this->saldo += $jumlah;
+return "Berhasil setor Rp " . number_format($jumlah);
+}
 
-    public function tarik($jumlah) {
-        if ($this->saldo >= $jumlah) {
-            $this->saldo -= $jumlah;
-            return "Berhasil tarik Rp " . number_format($jumlah);
-        }
-        return "Gagal! Saldo tidak cukup.";
-    }
+public function tarik($jumlah) {
+if ($this->saldo >= $jumlah) {
+$this->saldo -= $jumlah;
+return "Berhasil tarik Rp " . number_format($jumlah);
+}
+return "Gagal! Saldo tidak cukup.";
+}
 
-    public function getSaldo() {
-        return "Saldo Akhir: Rp " . number_format($this->saldo, 0, ',', '.');
-    }
+public function getSaldo() {
+return "Saldo Akhir: Rp " . number_format($this->saldo, 0, ',', '.');
+}
 }
 
 class Siswa extends Tabungan {
-    public function cetakIdentitas() {
-        return "<h3>Nama Siswa: $this->namaSiswa</h3>";
-    }
+public function cetakIdentitas() {
+return "
+
+Nama Siswa: $this->namaSiswa
+";
+}
 }
 
 // Simulasi Transaksi (Karena di browser tidak bisa input manual lewat fgets)
 $siswa1 = new Siswa("Siswa 1", 50000);
 echo $siswa1->cetakIdentitas();
-echo $siswa1->setor(20000) . "<br>";
-echo $siswa1->tarik(10000) . "<br>";
-echo $siswa1->getSaldo() . "<hr>";
+echo $siswa1->setor(20000) . "
+";
+echo $siswa1->tarik(10000) . "
+";
+echo $siswa1->getSaldo() . "
+";
 
 $siswa2 = new Siswa("Siswa 2", 100000);
 echo $siswa2->cetakIdentitas();
-echo $siswa2->tarik(120000) . "<br>"; // Contoh gagal tarik
+echo $siswa2->tarik(120000) . "
+"; // Contoh gagal tarik
 echo $siswa2->getSaldo();
-?>
